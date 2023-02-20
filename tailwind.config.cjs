4ef79eb568f3b214677708a242d04516703b48c1
p-kosinski <kosinskipiotr.98@gmail.com>
2023-02-20 19:25:06 +0100

    Add Tailwind theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{ts,tsx}'
-  ],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      'red': 'hsla(0, 90%, 63%, 1)',
+      'orange': 'hsla(13, 96%, 67%, 1)',
+      'yellow': 'hsla(42, 91%, 69%, 1)',
+      'green': 'hsla(127, 100%, 82%, 1)',
+      'light': 'hsla(252, 11%, 91%, 1)',
+      'grey-light': 'hsla(252, 9%, 53%, 1)',
+      'grey-dark': 'hsla(247, 11%, 15%, 1)',
+      'dark': 'hsla(248, 15%, 11%, 1)',
+    },
+    fontFamily: {
+      main: ['JetBrains Mono', 'monospace'],
+      italic: ['JetBrains Mono Italic', 'monospace'],
+    }
   },
   plugins: [],
 }
